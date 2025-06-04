83dc1607d1cf6d5e4c39b4d0a1565eab81dc065c
Vinish <Vinish@MacBook-Air.local>
2025-06-04 16:02:05 -0700

    frontend

--- backend/stockFetch.py
+++ backend/stockFetch.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import argparse
 import os
+
 import json
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
@@ -9,11 +10,11 @@ import finnhub
 from newspaper import Article
 from newsapi import NewsApiClient
 from google.cloud import language_v1
+
 # GNEWS_API_KEY = os.getenv('GNEWS_API_KEY')
-FINNHUB_API_KEY = os.getenv('FINNHUB_API_KEY')
+FINNHUB_API_KEY = os.getenv('FINNHUB_KEY')
 finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
 google_client = language_v1.LanguageServiceClient()
-
 # newsapi = NewsApiClient(api_key=GNEWS_API_KEY)
 # /v2/top-headlines
 
